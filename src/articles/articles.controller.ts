fab26f4671305db6c90e3813b25b8377970694b4
ahadiii786 <mhdahadsiddique@gmail.com>
2025-04-10 22:25:49 +0500

    Input Va;idation and Error Handling

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete , ParseIntPipe, NotFoundException,
+} from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
@@ -23,7 +24,6 @@ create(@Body() createArticleDto: CreateArticleDto) {
 
   @Get('drafts')
   @ApiOkResponse({ type: ArticleEntity, isArray: true })
-
   findDrafts() {
     return this.articlesService.findDrafts();
   }
@@ -31,27 +31,32 @@ create(@Body() createArticleDto: CreateArticleDto) {
 
   @Get()
   @ApiOkResponse({ type: ArticleEntity, isArray: true })
-
   findAll() {
     return this.articlesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.articlesService.findOne(+id);
+  @ApiOkResponse({ type: ArticleEntity })
+  async findOne(@Param('id') id: string) {
+    const article = await this.articlesService.findOne(+id);
+    if (!article) {
+      throw new NotFoundException(`Article with ${id} does not exist.`);
+    }
+    return article;
   }
 
   @Patch(':id')
-  @ApiOkResponse({ type: ArticleEntity })
-
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articlesService.update(+id, updateArticleDto);
+  @ApiCreatedResponse({ type: ArticleEntity })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateArticleDto: UpdateArticleDto,
+  ) {
+    return this.articlesService.update(id, updateArticleDto);
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: ArticleEntity })
-
-  remove(@Param('id') id: string) {
-    return this.articlesService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.remove(id);
   }
 }
